4ab3f439601b90b8225370bcf82aa42c02893f52
SeoYoung <87682189+0o1ong@users.noreply.github.com>
2024-07-10 15:36:07 +0900

    Update ResNet.py

--- ResNet/ResNet.py
+++ ResNet/ResNet.py
@@ -5,48 +5,52 @@ import torch.nn.functional as F
 class BasicBlock(nn.Module):
     def __init__(self, in_dim, dim, stride=1):
         super(BasicBlock, self).__init__()
-        self.bn1 = nn.BatchNorm2d(in_dim) 
-        self.conv1 = nn.Conv2d(in_dim, dim, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(dim)
-        self.conv2 = nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=1, bias=False)
-        self.dropout = nn.Dropout(0.2)
+
+        # full pre-activation: (BN -> ReLU -> conv -> BN -> ReLU -> conv) + x
+        self.residual = nn.Sequential(
+            nn.BatchNorm2d(in_dim),
+            nn.ReLU(),
+            nn.Conv2d(in_dim, dim, kernel_size=3, stride=stride, padding=1),
+            nn.Dropout(0.2),
+            nn.BatchNorm2d(dim),
+            nn.ReLU(),
+            nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=1),
+            nn.Dropout(0.2)
+        )
         
         # downsampling (in_dim != out_dim); identity mapping 바로 적용 불가
-        if stride == 1: self.identity = nn.Sequential()  
-        else: self.identity = nn.Conv2d(in_dim, dim, kernel_size=1, stride=stride, bias=False) # stride=2를 통해 feature map size 맞추기
+        self.identity = nn.Sequential()  
+        if stride != 1 or in_dim != dim:
+            self.identity = nn.Conv2d(in_dim, dim, kernel_size=1, stride=stride, bias=False) # stride를 통해 feature map size 맞추기
             
     def forward(self, x):
-        # full pre-activation: (BN -> ReLU -> conv -> BN -> ReLU -> conv) + x
-        out = F.relu(self.bn1(x))
-        out = self.conv1(out)
-        out = self.dropout(out)
-        out = F.relu(self.bn2(out))
-        out = self.conv2(out)
-        out = self.dropout(out)
-        out += self.identity(x)
-        return out
+        return self.residual(x) + self.identity(x)
 
 class ResNet(nn.Module):
     def __init__(self, block, block_num, num_classes=10):
         super(ResNet, self).__init__()
         self.in_dim = 16
-
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        
+        self.firstConv = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU()
+        )
         self.layer1 = self.residual_block(block, 16, block_num[0], first_stride=1)
         self.layer2 = self.residual_block(block, 32, block_num[1], first_stride=2)
         self.layer3 = self.residual_block(block, 64, block_num[2], first_stride=2)
-        self.fc = nn.Linear(64, num_classes) # fc layer
+        self.fc = nn.Linear(64, num_classes)
 
     def residual_block(self, block, dim, num_blocks, first_stride):
-        strides = [first_stride] + [1] * (num_blocks - 1) # stride가 맨 처음 conv 연산에만 적용되도록, 그 다음에는 전부 1(w, h 유지)
+        strides = [first_stride] + [1] * (num_blocks - 1)
         layers = []
         for stride in strides:
             layers.append(block(self.in_dim, dim, stride))
-            self.in_dim = dim # dim update (다음 레이어 in_dim)
+            self.in_dim = dim
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        x = self.conv1(x)
+        x = self.firstConv(x)
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
@@ -55,6 +59,5 @@ class ResNet(nn.Module):
         x = F.softmax(self.fc(x), dim=1)
         return x
 
-# ResNet56 함수 정의
 def get_model():
-    return ResNet(BasicBlock, [9, 9, 9]) # 각 레이어에 9개의 블록 사용
+    return ResNet(BasicBlock, [9, 9, 9])
