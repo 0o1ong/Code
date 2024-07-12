182a2b4569c0c753f4af0dd24112744b70775508
SeoYoung <87682189+0o1ong@users.noreply.github.com>
2024-07-12 10:39:40 +0900

    Update ResNet.py

--- ResNet/ResNet.py
+++ ResNet/ResNet.py
@@ -8,34 +8,38 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
 
         self.residual = nn.Sequential(
-            nn.Conv2d(in_dim, dim, kernel_size=3, padding=1),
+            nn.Conv2d(in_dim, dim, kernel_size=3, stride=stride, padding=1),
             nn.BatchNorm2d(dim),
             nn.ReLU(),
             nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(dim)
         )
-        self.downsample = nn.Conv2d(in_dim, dim, kernel_size=1) if in_dim != dim else nn.Sequential()
+        self.identity = nn.Sequential()
+        if stride != 1 or in_dim != dim:
+            self.identity = nn.Conv2d(in_dim, dim, kernel_size=1, stride=stride)
             
     def forward(self, x):
-        return F.relu(self.residual(x) + self.downsample(x))
+        return F.relu(self.residual(x) + self.identity(x))
 
 # Full pre-activation
 class PreActBlock(nn.Module):
-    def __init__(self, in_dim, dim):
+    def __init__(self, in_dim, dim, stride=1):
         super(PreActBlock, self).__init__()
 
         self.residual = nn.Sequential(
             nn.BatchNorm2d(in_dim),
             nn.ReLU(),
-            nn.Conv2d(in_dim, dim, kernel_size=3, padding=1),
+            nn.Conv2d(in_dim, dim, kernel_size=3, stride=stride, padding=1),
             nn.BatchNorm2d(dim),
             nn.ReLU(),
-            nn.Conv2d(dim, dim, kernel_size=3, padding=1)
+            nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=1)
         )
-        self.downsample = nn.Conv2d(in_dim, dim, kernel_size=1) if in_dim != dim else nn.Sequential()
+        self.identity = nn.Sequential()
+        if stride != 1 or in_dim != dim:
+            self.identity = nn.Conv2d(in_dim, dim, kernel_size=1, stride=stride)
 
     def forward(self, x):
-        return self.residual(x) + self.downsample(x)
+        return self.residual(x) + self.identity(x)
 
 
 class ResNet(nn.Module):
@@ -45,18 +49,19 @@ class ResNet(nn.Module):
         self.block = block
         
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
-        self.layer1 = self.stage(block, 64, block_num[0])
-        self.layer2 = self.stage(block, 128, block_num[1])
-        self.layer3 = self.stage(block, 256, block_num[2])
-        self.layer4 = self.stage(block, 512, block_num[3])
         self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self.stage(block, 64, block_num[0], first_stride=1)
+        self.layer2 = self.stage(block, 128, block_num[1], first_stride=2)
+        self.layer3 = self.stage(block, 256, block_num[2], first_stride=2)
+        self.layer4 = self.stage(block, 512, block_num[3], first_stride=2)
         self.bn2 = nn.BatchNorm2d(512)
         self.fc = nn.Linear(512, num_classes) 
 
-    def stage(self, block, dim, num_blocks):
+    def stage(self, block, dim, num_blocks, first_stride):
+        strides = [first_stride] + [1]*(num_blocks-1)
         layers = []
-        for _ in range(num_blocks):
-            layers.append(block(self.in_dim, dim))
+        for stride in strides:
+            layers.append(block(self.in_dim, dim, stride))
             self.in_dim = dim
         return nn.Sequential(*layers)
 
@@ -66,11 +71,8 @@ class ResNet(nn.Module):
             x = self.bn1(x)
             x = F.relu(x)
         x = self.layer1(x)
-        x = F.avg_pool2d(x, 2) # stride=2 -> pooling
         x = self.layer2(x)
-        x = F.avg_pool2d(x, 2)
         x = self.layer3(x)
-        x = F.avg_pool2d(x, 2)
         x = self.layer4(x)
         # Pre-act의 경우 last residual block; extra activation
         if self.block == PreActBlock:
@@ -82,5 +84,5 @@ class ResNet(nn.Module):
         return x
 
 def get_model():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
-    # return ResNet(PreActBlock, [2, 2, 2, 2]) 
+    # return ResNet(BasicBlock, [2, 2, 2, 2])
+    return ResNet(PreActBlock, [2, 2, 2, 2]) 
