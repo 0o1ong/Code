daf293de7c86ae70fe53461c7db7fc03d47d2d6f
SeoYoung <87682189+0o1ong@users.noreply.github.com>
2024-07-18 20:54:05 +0900

    Update main.py

--- SSL/main.py
+++ SSL/main.py
@@ -1,24 +1,28 @@
+import os
 import argparse
 
 from model import PreActBlock, ResNet
 from datasets import get_data_loaders
 from train import train
 
-def main(args):
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--learning_rate', type=float, default=0.1)
+    parser.add_argument('--epoch_num', type=int, default=300)
+    parser.add_argument('--logdir', type=str, default='log')
+    parser.add_argument('--dataset', type=str, default='CIFAR10')
+    parser.add_argument('--version', type=str, default='v1') # v1 / v2 / v3
+    
+    args = parser.parse_args()
+
+    if not os.path.exists(os.path.join(logdir, version)):
+        os.makedirs(os.path.join(logdir, version))
 
     pre_train_model = ResNet(PreActBlock, [2, 2, 2, 2], args.version)
     train_loader, test_loader = get_data_loaders(args.batch_size, args.dataset)
     
     train(pre_train_model, train_loader, test_loader, args.epoch_num, args.learning_rate, args.logdir, args.version)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--batch_size', type=int, default=128)
-    parser.add_argument('--learning_rate', type=float, default=0.1)
-    parser.add_argument('--epoch_num', type=int, default=3)
-    parser.add_argument('--logdir', type=str, default='log')
-    parser.add_argument('--dataset', type=str, default='CIFAR10')
-    parser.add_argument('--version', type=str, default='v3') # v1 / v2 / v3
-    args = parser.parse_args()
-
-    main(args)
+if __name__ == '__main__': 
+    main()
