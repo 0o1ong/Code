1244b2abea5b1d8f5bf4e4e4c05442cd261165fb
SeoYoung <87682189+0o1ong@users.noreply.github.com>
2024-07-21 14:36:08 +0900

    Update main.py

--- SSL/main.py
+++ SSL/main.py
@@ -1,28 +1,34 @@
 import os
 import argparse
 
-from model import PreActBlock, ResNet
+from models import *
 from datasets import get_data_loaders
-from train import train
+from train import *
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--learning_rate', type=float, default=0.1)
-    parser.add_argument('--epoch_num', type=int, default=300)
+    parser.add_argument('--epoch_num', type=int, default=100)
     parser.add_argument('--logdir', type=str, default='log')
-    parser.add_argument('--dataset', type=str, default='CIFAR10')
-    parser.add_argument('--version', type=str, default='v1') # v1 / v2 / v3
+    parser.add_argument('--model', type=str, default='vit')
+    parser.add_argument('--dataset', type=str, default='cifar10')
+    parser.add_argument('--train', type=str, default='basic')
+    parser.add_argument('--version', type=str, default='v1') # for --train rotation (v1 / v2 / v3)
     
     args = parser.parse_args()
 
-    if not os.path.exists(os.path.join(logdir, version)):
-        os.makedirs(os.path.join(logdir, version))
+    if not os.path.exists(os.path.join(args.logdir, args.version)):
+        os.makedirs(os.path.join(args.logdir, args.version))
 
-    pre_train_model = ResNet(PreActBlock, [2, 2, 2, 2], args.version)
     train_loader, test_loader = get_data_loaders(args.batch_size, args.dataset)
-    
-    train(pre_train_model, train_loader, test_loader, args.epoch_num, args.learning_rate, args.logdir, args.version)
 
+    if args.model == 'rotnet':
+        model = get_model(args.model, PreActBlock, [2, 2, 2, 2], args.version)
+        train(args.train, model, train_loader, test_loader, args.epoch_num, args.learning_rate, args.logdir, args.version)
+    else:
+        model = get_model(args.model)
+        train(args.train, model, train_loader, test_loader, args.epoch_num, args.learning_rate, args.logdir)
+        
 if __name__ == '__main__': 
     main()
