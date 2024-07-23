6ab8b1205645c51cce6e39c3236e3e10efdeae3a
SeoYoung <87682189+0o1ong@users.noreply.github.com>
2024-07-23 11:35:27 +0900

    Update __init__.py

--- SSL/train/__init__.py
+++ SSL/train/__init__.py
@@ -1,13 +1,16 @@
 from .trainRotNet import *
 from .trainBasic import *
+from .SimCLR import *
 from .utils import *
 
-__all__ = ['train_rotnet', 'trainBasic', 'train']
+__all__ = ['train_rotnet', 'simclr', 'trainBasic', 'train']
 
 def train(train_type, *args):
     if train_type == 'rotation':
         return train_rotnet(*args)
+    elif train_type == 'simclr':
+        return simclr(*args)
     elif train_type == 'basic':
         return train_basic(*args)
     else:
-        raise ValueError(f"Unknown model: {train_type}")
+        raise ValueError(f"Unknown train type: {train_type}")
