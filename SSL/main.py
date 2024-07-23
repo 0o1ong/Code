57be50b18d23a9d43bdff825534da87764dc7031
SeoYoung <87682189+0o1ong@users.noreply.github.com>
2024-07-23 10:49:03 +0900

    Update main.py

--- SSL/main.py
+++ SSL/main.py
@@ -7,32 +7,33 @@ from train import *
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--batch_size', type=int, default=512)
     parser.add_argument('--learning_rate', type=float, default=1e-3)
     parser.add_argument('--epoch_num', type=int, default=100)
-    parser.add_argument('--logdir', type=str, default='log_vit')
-    parser.add_argument('--model', type=str, default='vit')
+    parser.add_argument('--logdir', type=str, default='log_simclr')
+    parser.add_argument('--model', type=str, default='resnet')
     parser.add_argument('--dataset', type=str, default='cifar10')
-    parser.add_argument('--train', type=str, default='basic')
+    parser.add_argument('--train', type=str, default='simclr')
+    
     parser.add_argument('--version', type=str, default='v1') # for --train rotation (v1 / v2 / v3)
     
     args = parser.parse_args()
 
-    if args.model == 'rotnet':
-        if not os.path.exists(os.path.join(args.logdir, args.version)):
-            os.makedirs(os.path.join(args.logdir, args.version))
-    else:
-        if not os.path.exists(args.logdir):
-            os.makedirs(args.logdir)
+    if not os.path.exists(os.path.join(args.logdir)):
+        os.makedirs(os.path.join(args.logdir))
 
-    train_loader, test_loader = get_data_loaders(args.batch_size, args.dataset)
+    train_loader, test_loader = get_data_loaders(args.batch_size, args.dataset, args.train)
 
     if args.model == 'rotnet':
         model = get_model(args.model, PreActBlock, [2, 2, 2, 2], args.version)
         train(args.train, model, train_loader, test_loader, args.epoch_num, args.learning_rate, args.logdir, args.version)
     else:
-        model = get_model(args.model)
-        train(args.train, model, train_loader, test_loader, args.epoch_num, args.learning_rate, args.logdir)
+        if args.train == 'simclr':
+            model = get_model(args.model, BottleNeck, [3, 4, 6, 3]) # Base Encoder: ResNet-50
+            train(args.train, model, train_loader, test_loader, args.epoch_num, args.learning_rate, args.logdir)
+        else:
+            model = get_model(args.model)
+            train(args.train, model, train_loader, test_loader, args.epoch_num, args.learning_rate, args.logdir)
         
 if __name__ == '__main__': 
     main()
