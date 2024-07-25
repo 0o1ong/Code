c5ba10904077bdcb7270b41b5f06afa38f7d7c1d
SeoYoung <87682189+0o1ong@users.noreply.github.com>
2024-07-25 19:22:39 +0900

    Update __init__.py

--- SSL/train/__init__.py
+++ SSL/train/__init__.py
@@ -1,15 +1,17 @@
-from .trainRotNet import *
-from .trainBasic import *
-from .SimCLR import *
-from .utils import *
+from .trainRotNet import train_rotnet
+from .trainBasic import train_basic
+from .SimCLR import simclr
+from .MoCo import moco
 
-__all__ = ['train_rotnet', 'simclr', 'trainBasic', 'train']
+__all__ = ['train_rotnet', 'simclr', 'moco', 'trainBasic', 'linear_acc', 'train']
 
 def train(train_type, *args):
     if train_type == 'rotation':
         return train_rotnet(*args)
     elif train_type == 'simclr':
         return simclr(*args)
+    elif train_type == 'moco':
+        return moco(*args)
     elif train_type == 'basic':
         return train_basic(*args)
     else:
