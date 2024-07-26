5b010dc40a8f142bb39ced52455aa83c7c76e061
SeoYoung <87682189+0o1ong@users.noreply.github.com>
2024-07-26 21:42:19 +0900

    Update main.py

--- SSL/main.py
+++ SSL/main.py
@@ -6,17 +6,17 @@ import argparse
 from models import get_model, BasicBlock, PreActBlock, BottleNeck
 from datasets import get_data_loaders
 from train import train
-from utils import log_setting, lr_scheduler
+from utils import log_setting, LinearWarmupCosineAnnealingLR
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batch_size', type=int, default=256)
-    parser.add_argument('--learning_rate', type=float, default=0.03)
-    parser.add_argument('--epoch_num', type=int, default=1)
+    parser.add_argument('--batch_size', type=int, default=512)
+    parser.add_argument('--learning_rate', type=float, default=1e-3)
+    parser.add_argument('--epoch_num', type=int, default=200)
     parser.add_argument('--logdir', type=str, default='log') 
-    parser.add_argument('--model', type=str, default='rotnet')
+    parser.add_argument('--model', type=str, default='resnet')
     parser.add_argument('--dataset', type=str, default='cifar10')
-    parser.add_argument('--train', type=str, default='rotation')
+    parser.add_argument('--train', type=str, default='simclr')
     
     parser.add_argument('--version', type=str, default='v1') # for --train rotation (v1 / v2 / v3)
     
@@ -27,7 +27,7 @@ def main():
         train_loader, test_loader = get_data_loaders(args.batch_size, args.dataset, args.train)
 
     log_setting(args.logdir)
-    device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     criterion = nn.CrossEntropyLoss()
 
     if args.model == 'rotnet':
@@ -38,15 +38,25 @@ def main():
                                                     gamma=0.2)
         train(args.train, model, train_loader, test_loader, optimizer, criterion, lr_scheduler, device, args.epoch_num, args.logdir, args.version)
     else:
-        if args.train == 'simclr' or args.train == 'moco':
+        if args.train == 'simclr':
             model = get_model(args.model, BottleNeck, [3, 4, 6, 3]).to(device) # Base Encoder: ResNet-50
             optimizer = optim.SGD(model.parameters(), weight_decay=1e-4, momentum=0.9, nesterov=True, lr=args.learning_rate, dampening=False)
-            lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epoch_num)
+            lr_scheduler = LinearWarmupCosineAnnealingLR(optimizer, warmup_steps=10, total_steps=args.epoch_num)
             train(args.train, model, train_loader, test_loader, pretrain_loader, optimizer, lr_scheduler, device, args.epoch_num, args.logdir)
+        elif args.train == 'moco':
+            # batch_size = 256
+            f_q = get_model(args.model, BottleNeck, [3, 4, 6, 3], num_classes=128).to(device) # Query Encoder
+            f_k = get_model(args.model, BottleNeck, [3, 4, 6, 3], num_classes=128).to(device) # Key Encoder
+            optimizer = optim.SGD(f_q.parameters(), weight_decay=1e-4, momentum=0.9, lr=0.03)
+            lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, 
+                                                  milestones=[120, 160], 
+                                                  gamma=0.1)
+            train(args.train, f_q, f_k, train_loader, test_loader, pretrain_loader, optimizer, criterion, lr_scheduler, device, args.epoch_num, args.logdir)
         else:
             model = get_model(args.model).to(device)
             optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
             lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epoch_num)
+            # lr_scheduler = LinearWarmupCosineAnnealingLR(optimizer, warmup_steps=100, epoch_num=args.epoch_num)
             train(args.train, model, train_loader, test_loader, optimizer, criterion, lr_scheduler, device, args.epoch_num, args.logdir)
         
 if __name__ == '__main__': 
