70af2aa3c766e0e17c028b20b8cbc48d4eb694f9
SeoYoung <87682189+0o1ong@users.noreply.github.com>
2024-07-27 14:12:13 +0900

    Update MoCo.py

--- SSL/train/MoCo.py
+++ SSL/train/MoCo.py
@@ -1,38 +1,39 @@
 import torch
+import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
 from .utils import save_log, save_model, KNN_acc, linear_acc
 
 def moco(f_q, f_k, train_loader, test_loader, pretrain_loader, optimizer, criterion, lr_scheduler, device, epoch_num=200, logdir='log_moco'):
-    batch_size=256
+    # batch_size=256
     dim=128
     dict_size=16384
     m=0.999
     t=0.07
-
-    queue = torch.randn(dict_size, dim).to(device) # (64*256, 128) 랜덤 초기화 (dict_size = 64 * batch_size)
     
+    queue = torch.randn(dict_size, dim).to(device) # (64*256, 128) 랜덤 초기화 (dict_size = 64 * batch_size)
+
     for q_param, k_param in zip(f_q.parameters(), f_k.parameters()):
-        k_param.data.copy_(q_param)
+        k_param.data.copy_(q_param.data)
         k_param.requires_grad = False
 
     best_knn_acc = 0.0
     writer = SummaryWriter(f'{logdir}')
     
     for epoch in range(epoch_num):
-
         f_q.train(True)
         running_loss = 0.0
+
         for inputs, _ in pretrain_loader:
             x_q, x_k = inputs
             x_q, x_k = x_q.to(device), x_k.to(device) # Positive Pair
-
-            # 마지막 배치 크기: 80 (!= 256)
-            current_batch_size = x_q.size(0)
-
+            current_batch_size = x_q.size(0) # 마지막 배치 크기: 80 (!= 256)
+            
             optimizer.zero_grad()
             
             q = f_q(x_q) # (batch size, dim) -> (256, 128), 128차원의 쿼리 256개
+            q = F.normalize(q, dim=1)
             k = f_k(x_k) # 128차원의 키 256개
+            k = F.normalize(k, dim=1)
             k = k.detach() # no gradient to keys
 
             # positive logits
@@ -42,25 +43,26 @@ def moco(f_q, f_k, train_loader, test_loader, pretrain_loader, optimizer, criter
             # negative logits
             # matrix multiplication -> 쿼리 & 딕셔너리의 키값들과의 유사도 (q*k)
             l_neg = torch.matmul(q.view(current_batch_size, dim), queue.T) # (current_batch_size, dict_size) -> batch_size개의 쿼리와 dict_size개의 키끼리 유사도 (l_neg[i][j]: q_i * k_j)
-            logits = torch.cat([l_pos.view(-1, 1), l_neg], dim=-1) # (current_batch_size, dict_size+1)
+            logits = torch.cat([l_pos.view(current_batch_size, 1), l_neg], dim=-1) # (current_batch_size, dict_size+1)
 
             labels = torch.zeros(current_batch_size, dtype=torch.long).to(device) # positive -> 1st column (idx=0)
             loss = criterion(logits/t, labels)
             loss.backward()
-            optimizer.step()
+            optimizer.step() # f_q update
 
             running_loss += loss.item()
 
             # Momentum update
-            for q_param, k_param in zip(f_q.parameters(), f_k.parameters()):
-                k_param.data.copy_(m*k_param + (1.0-m)*q_param)
+            with torch.no_grad():
+                for q_param, k_param in zip(f_q.parameters(), f_k.parameters()):
+                    k_param.data.copy_(m * k_param.data + (1.0 - m) * q_param.data)
 
             # queue.size(): (dict_size, dim)
             queue = torch.cat([queue[current_batch_size:], k])
 
         lr_scheduler.step()
 
-        train_loss = running_loss / len(train_loader)
+        train_loss = running_loss / len(pretrain_loader)
         writer.add_scalar('Loss/train', train_loss, epoch)
 
         # Calculate KNN accuracy
@@ -72,5 +74,4 @@ def moco(f_q, f_k, train_loader, test_loader, pretrain_loader, optimizer, criter
             save_model(knn_acc, f_q, logdir, epoch)
             best_knn_acc = knn_acc
     linear_acc(f_q, epoch_num, 2048, 10, train_loader, test_loader, device)
-    
     writer.close()
