43068350a07023a61332aa43e16cf54d4fe013bf
SeoYoung <87682189+0o1ong@users.noreply.github.com>
2024-07-28 15:40:48 +0900

    Update utils.py

--- SSL/utils.py
+++ SSL/utils.py
@@ -1,5 +1,7 @@
 import os
+import math
 import logging
+from torch.optim.lr_scheduler import LambdaLR
 
 def log_setting(logdir):
     if not os.path.exists(os.path.join(logdir)):
@@ -11,5 +13,19 @@ def log_setting(logdir):
                         logging.StreamHandler()
                         ])
 
-class lr_scheduler():
-    def __init__(self, warmup, decay, steps, min_lr, max_lr):
+class LinearWarmupCosineAnnealingLR():
+    def __init__(self, optimizer, warmup_steps, total_steps):
+        self.optimizer = optimizer
+        self.warmup_steps = warmup_steps
+        self.total_steps = total_steps
+        self.scheduler = LambdaLR(optimizer, lr_lambda=self.schedule)
+
+    def schedule(self, epoch):
+        if epoch <= self.warmup_steps:
+            return epoch / self.warmup_steps
+        else:
+            t = (epoch - self.warmup_steps) / (self.total_steps - self.warmup_steps)
+            return 0.5 * (1 + math.cos(math.pi * t))
+    
+    def step(self):
+        self.scheduler.step()
