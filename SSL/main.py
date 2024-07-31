1916220fdab93775b198a492beb20cd2b176681e
SeoYoung <87682189+0o1ong@users.noreply.github.com>
2024-07-31 09:25:53 +0900

    Update main.py

--- SSL/main.py
+++ SSL/main.py
@@ -8,15 +8,16 @@ from datasets import get_data_loaders
 from train import train
 from utils import log_setting, LinearWarmupCosineAnnealingLR
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batch_size', type=int, default=256)
-    parser.add_argument('--learning_rate', type=float, default=0.003)
-    parser.add_argument('--epoch_num', type=int, default=200)
-    parser.add_argument('--logdir', type=str, default='log_moco') 
-    parser.add_argument('--model', type=str, default='resnet')
+    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--learning_rate', type=float, default=1e-3)
+    parser.add_argument('--epoch_num', type=int, default=100)
+    parser.add_argument('--logdir', type=str, default='log')
+    parser.add_argument('--model', type=str, default='vit')
     parser.add_argument('--dataset', type=str, default='cifar10')
-    parser.add_argument('--train', type=str, default='moco')
+    parser.add_argument('--train', type=str, default='basic')
     
     parser.add_argument('--version', type=str, default='v1') # for --train rotation (v1 / v2 / v3)
     
@@ -33,30 +34,25 @@ def main():
     if args.model == 'rotnet':
         model = get_model(args.model, PreActBlock, [2, 2, 2, 2], args.version).to(device)
         optimizer = optim.SGD(model.parameters(), weight_decay=5e-4, momentum=0.9, lr=args.learning_rate)
-        lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer,
-                                                    milestones=[30, 60, 80],
-                                                    gamma=0.2)
+        lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30, 60, 80], gamma=0.2)
         train(args.train, model, train_loader, test_loader, optimizer, criterion, lr_scheduler, device, args.epoch_num, args.logdir, args.version)
     else:
         if args.train == 'simclr':
-            model = get_model(args.model, BottleNeck, [3, 4, 6, 3]).to(device) # Base Encoder: ResNet-50
-            optimizer = optim.SGD(model.parameters(), weight_decay=1e-4, momentum=0.9, nesterov=True, lr=args.learning_rate, dampening=False)
-            lr_scheduler = LinearWarmupCosineAnnealingLR(optimizer, warmup_steps=10, total_steps=args.epoch_num)
+            model = get_model(args.model, PreActBlock, [2, 2, 2, 2], 512).to(device) # Base Encoder: ResNet-18
+            optimizer = optim.SGD(model.parameters(), weight_decay=1e-6, momentum=0.9, nesterov=True, lr=args.learning_rate)
+            lr_scheduler = LinearWarmupCosineAnnealingLR(optimizer, warmup_steps=10, total_steps=args.epoch_num) # log_simclr_1
+            # lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epoch_num, eta_min=0.001) # log_simclr_2
             train(args.train, model, train_loader, test_loader, pretrain_loader, optimizer, lr_scheduler, device, args.epoch_num, args.logdir)
         elif args.train == 'moco':
-            # batch_size = 256, epoch_num=200  
             f_q = get_model(args.model, PreActBlock, [2, 2, 2, 2], 512).to(device) # Query Encoder
             f_k = get_model(args.model, PreActBlock, [2, 2, 2, 2], 512).to(device) # Key Encoder
-            optimizer = optim.SGD(f_q.parameters(), weight_decay=1e-4, momentum=0.9, lr=args.learning_rate)
-            lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, 
-                                                  milestones=[120, 160], 
-                                                  gamma=0.1)
+            optimizer = optim.SGD(f_q.parameters(), weight_decay=5e-4, momentum=0.9, lr=args.learning_rate)
+            lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, args.epoch_num)
             train(args.train, f_q, f_k, train_loader, test_loader, pretrain_loader, optimizer, criterion, lr_scheduler, device, args.epoch_num, args.logdir)
         else:
             model = get_model(args.model).to(device)
-            optimizer = optim.AdamW(model.parameters(), lr=args.learning_rate)
-            lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epoch_num)
-            # lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100, 150], gamma=0.1) # for vit
+            optimizer = optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=5e-5)
+            lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epoch_num, eta_min=1e-5)
             train(args.train, model, train_loader, test_loader, optimizer, criterion, lr_scheduler, device, args.epoch_num, args.logdir)
         
 if __name__ == '__main__': 
