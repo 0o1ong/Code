2447df931984ebebceb3fb478084f93a35e833e1
SeoYoung <87682189+0o1ong@users.noreply.github.com>
2024-08-07 09:16:20 +0900

    Update main.py\

--- SSL/main.py
+++ SSL/main.py
@@ -11,7 +11,7 @@ from utils import log_setting, LinearWarmupCosineAnnealingLR
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch_size', type=int, default=512)
-    parser.add_argument('--learning_rate', type=float, default=0.6)
+    parser.add_argument('--learning_rate', type=float, default=1e-3)
     parser.add_argument('--epoch_num', type=int, default=100)
     parser.add_argument('--logdir', type=str, default='cifar10_resnet18_simclr')
     parser.add_argument('--model', type=str, default='vit')
