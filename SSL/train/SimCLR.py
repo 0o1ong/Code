aa56360db4ec3ed4abd8a6c1b058120d52c694e5
SeoYoung <87682189+0o1ong@users.noreply.github.com>
2024-08-28 15:43:10 +0900

    Update SimCLR.py

--- SSL/train/SimCLR.py
+++ SSL/train/SimCLR.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
+
 from .utils import save_log, save_model, KNN_acc
 
 # NT-Xent loss function
-def NT_Xent(z, temperature, device): # z.size(): (2batch_size, 512)
+def NT_Xent(z, temperature, device): # z.size(): (2batch_size, out_dim)
     z = F.normalize(z, dim=1)
     cos_sim = torch.matmul(z, z.T) / temperature
     cos_sim.fill_diagonal_(float('-inf')) # 같은 이미지에 대한 유사도(sim_{i, i}) 무시 가능
@@ -25,14 +26,15 @@ def simclr(model, empty_model, train_loader, test_loader, pretrain_loader, optim
             inputs1, inputs2 = inputs1.to(device), inputs2.to(device)
 
             optimizer.zero_grad()
-            out1 = model(inputs1)  # (batch_size, 512)
-            out2 = model(inputs2)  # (batch_size, 512)
-            z = torch.cat([out1, out2]) # i, i + batch_size -> positive pair, (2*batch_size, 512)
+            out1 = model(inputs1)  # (batch_size, out_dim)
+            out2 = model(inputs2)  # (batch_size, out_dim)
+            z = torch.cat([out1, out2]) # i, i + batch_size -> positive pair, (2*batch_size, out_dim)
             loss = NT_Xent(z=z, temperature=0.5, device=device)
             loss.backward()
             optimizer.step()
 
             running_loss += loss.item()
+        
         lr_scheduler.step()
 
         train_loss = running_loss / len(train_loader)
@@ -46,5 +48,5 @@ def simclr(model, empty_model, train_loader, test_loader, pretrain_loader, optim
         if knn_acc > best_knn_acc:
             save_model(knn_acc, model, logdir, epoch)
             best_knn_acc = knn_acc
-            
+    # linear_acc(model, epoch_num, out_dim, 10, train_loader, test_loader, device)
     writer.close()
