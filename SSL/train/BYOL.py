9a489367905bb0c9e8fe7f04878af0b9dbc65a85
SeoYoung <87682189+0o1ong@users.noreply.github.com>
2024-08-28 15:42:29 +0900

    Update BYOL.py

--- SSL/train/BYOL.py
+++ SSL/train/BYOL.py
@@ -1,34 +1,19 @@
+import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
 from .utils import save_log, save_model, KNN_acc
 
-class Predictor(nn.Module):
-    def __init__(self, in_dim=512, hidden_dim=2048, out_dim=512):
-        super().__init__()
-        self.fc = nn.Sequential(nn.Linear(in_dim, hidden_dim),
-                                    nn.BatchNorm1d(hidden_dim),
-                                    nn.ReLU(),
-                                    nn.Linear(hidden_dim, out_dim))
-    def forward(self, x):
-        return self.fc(x)
-
 def mse_loss(x, y):
-    x = F.normalize(x)
-    y = F.normalize(y)
-    return 2 - 2 * (x * y).sum(dim=-1).mean()
+    x = F.normalize(x, dim=-1, p=2)
+    y = F.normalize(y, dim=-1, p=2)
+    return 2 - 2 * (x * y).sum(dim=-1)
 
-def byol(backbone, target, train_loader, test_loader, pretrain_loader, optimizer, criterion, lr_scheduler, device, epoch_num, logdir):
-    tau_start=0.99
-    online = nn.Sequential(backbone, Predictor().to(device)) # add prediction layer
-
-    ###
-    optimizer = optim.AdamW(online.parameters(), weight_decay=0.05, lr=1e-3)
-    lr_scheduler = LinearWarmupCosineAnnealingLR(optimizer, warmup_steps=10, total_steps=epoch_num)
-    ###
-
-    for online_param, target_param in zip(backbone.parameters(), target.parameters()):
+def byol(online, target, train_loader, test_loader, pretrain_loader, optimizer, criterion, lr_scheduler, device, epoch_num, logdir):
+    tau=0.99
+    
+    for online_param, target_param in zip(online.encoder.parameters(), target.parameters()):
         target_param.data.copy_(online_param.data)
         target_param.requires_grad = False
     
@@ -38,7 +23,6 @@ def byol(backbone, target, train_loader, test_loader, pretrain_loader, optimizer
     for epoch in range(epoch_num):
         online.train(True)
         running_loss = 0.0
-        tau = tau_start + (1-tau_start)*(epoch/epoch_num) # 0.99 ~ 1
 
         for inputs, _ in pretrain_loader:
             input1, input2 = inputs
@@ -49,14 +33,14 @@ def byol(backbone, target, train_loader, test_loader, pretrain_loader, optimizer
             pred1, pred2 = online(input1), online(input2)
             targ1, targ2 = target(input2).detach(), target(input1).detach()
 
-            loss = mse_loss(pred1, targ1) + mse_loss(pred2, targ2)
+            loss = (mse_loss(pred1, targ1) + mse_loss(pred2, targ2)).mean()
             loss.backward()
             optimizer.step() # online network update
 
             running_loss += loss.item()
 
             # EMA
-            for online_param, target_param in zip(backbone.parameters(), target.parameters()):
+            for online_param, target_param in zip(online.encoder.parameters(), target.parameters()):
                 target_param.data.mul_(tau).add_(online_param.data, alpha=1-tau)
 
         lr_scheduler.step()
@@ -65,7 +49,7 @@ def byol(backbone, target, train_loader, test_loader, pretrain_loader, optimizer
         writer.add_scalar('Loss/train', train_loss, epoch)
 
         # Calculate KNN accuracy
-        knn_acc = KNN_acc(backbone, train_loader, test_loader, device)
+        knn_acc = KNN_acc(online.encoder, train_loader, test_loader, device)
         writer.add_scalar('Accuracy/KNN', knn_acc, epoch)
 
         save_log(epoch, epoch_num, train_loss, knn_acc=knn_acc)
