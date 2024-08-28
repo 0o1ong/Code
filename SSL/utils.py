c71d370bb6f1c97d10f8df543ba3607e4b513504
SeoYoung <87682189+0o1ong@users.noreply.github.com>
2024-08-28 15:23:51 +0900

    Update utils.py

--- SSL/utils.py
+++ SSL/utils.py
@@ -1,6 +1,7 @@
 import os
 import math
 import logging
+import torch.nn as nn
 from torch.optim.lr_scheduler import LambdaLR
 
 def log_setting(logdir):
@@ -18,10 +19,30 @@ class LinearWarmupCosineAnnealingLR(LambdaLR):
         self.warmup_steps = warmup_steps
         self.total_steps = total_steps
         super().__init__(optimizer, lr_lambda=self.schedule)
-         
+
     def schedule(self, step):
         if step <= self.warmup_steps:
             return step / self.warmup_steps
         else:
             t = (step - self.warmup_steps) / (self.total_steps - self.warmup_steps)
             return 0.5 * (1 + math.cos(math.pi * t))
+
+class MLP(nn.Module):
+    def __init__(self, in_dim=512, hidden_dim=2048, out_dim=512):
+        super().__init__()
+        self.fc = nn.Sequential(nn.Linear(in_dim, hidden_dim),
+                                    nn.BatchNorm1d(hidden_dim),
+                                    nn.ReLU(),
+                                    nn.Linear(hidden_dim, out_dim))
+    def forward(self, x):
+        return self.fc(x)
+
+class Module(nn.Module):
+    def __init__(self, backbone, projector, predictor):
+        super().__init__()
+        self.backbone = backbone
+        self.projector = projector
+        self.predictor = predictor
+    
+    def get_module(self):
+        return nn.Sequential(self.backbone, self.projector, self.predictor)
