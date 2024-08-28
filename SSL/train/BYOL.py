0562c358415de306c3175d8a5417ad5c3b918223
SeoYoung <87682189+0o1ong@users.noreply.github.com>
2024-08-28 11:38:03 +0900

    Update BYOL.py

--- SSL/train/BYOL.py
+++ SSL/train/BYOL.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
 from .utils import save_log, save_model, KNN_acc
